a9df568d782e389d4dde35a8ec46206b8236a46b
songmoro <wlqgkr@naver.com>
2025-07-31 15:50:14 +0900

    Initial Commit

---
+++ TMDBPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBPedia
+//
+//  Created by 송재훈 on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
