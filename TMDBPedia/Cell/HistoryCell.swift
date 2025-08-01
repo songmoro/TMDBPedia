cc46134fbcc00c36f655017a55f87b0422e438fa
songmoro <wlqgkr@naver.com>
2025-08-02 01:13:16 +0900

    뷰 컨트롤러 폴더링 | 셀 분리

---
+++ TMDBPedia/Cell/HistoryCell.swift
@@ -0,0 +1,115 @@
+//
+//  HistoryCell.swift
+//  TMDBPedia
+//
+//  Created by 송재훈 on 8/2/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+// MARK: -HistoryCell-
+final class HistoryCell: UITableViewCell, IsIdentifiable {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureSubview()
+        configureLayout()
+        configureView()
+        configureCollectionView()
+    }
+    
+    private func configureSubview() {
+        contentView.addSubview(collectionView)
+    }
+    
+    private func configureLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func configureView() {
+        
+    }
+}
+
+extension HistoryCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    private func configureCollectionView() {
+        collectionView.do {
+            let layout = UICollectionViewFlowLayout().then {
+                $0.itemSize = CGSize(width: 60, height: 30)
+                $0.minimumLineSpacing = CGFloat(Constant.offsetFromHorizon)
+                $0.minimumInteritemSpacing = 0
+                $0.scrollDirection = .horizontal
+            }
+            
+            $0.delegate = self
+            $0.dataSource = self
+            $0.register(HistoryContentCell.self)
+            $0.collectionViewLayout = layout
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(HistoryContentCell.self, for: indexPath)
+        
+        return cell
+    }
+}
+// MARK: -
+
+// MARK: -HistoryContentCell-
+final class HistoryContentCell: UICollectionViewCell, IsIdentifiable {
+    let keywordLabel = UILabel()
+    let deleteButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureSubview()
+        configureLayout()
+        configureView()
+    }
+    
+    private func configureSubview() {
+        contentView.addSubviews(keywordLabel, deleteButton)
+    }
+    
+    private func configureLayout() {
+        keywordLabel.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+        }
+        
+        deleteButton.snp.makeConstraints {
+            $0.trailing.centerY.equalToSuperview()
+        }
+    }
+    
+    private func configureView() {
+        keywordLabel.text = "하얼빈"
+        deleteButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+    }
+}
+// MARK: -
---
+++ TMDBPedia/Cell/TodayMovieCell.swift
@@ -0,0 +1,153 @@
+//
+//  TodayMovieCell.swift
+//  TMDBPedia
+//
+//  Created by 송재훈 on 8/2/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+// MARK: -MovieCell-
+final class TodayMovieCell: UITableViewCell, IsIdentifiable {
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureSubview()
+        configureLayout()
+        configureView()
+        configureCollectionView()
+    }
+    
+    private func configureSubview() {
+        contentView.addSubview(collectionView)
+    }
+    
+    private func configureLayout() {
+        collectionView.snp.makeConstraints {
+            $0.size.equalToSuperview()
+        }
+    }
+    
+    private func configureView() {
+        
+    }
+}
+
+extension TodayMovieCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    private func configureCollectionView() {
+        collectionView.do {
+            let layout = UICollectionViewFlowLayout().then {
+                let bounds = UIScreen.main.bounds
+                $0.itemSize = CGSize(width: bounds.width * 0.6, height: bounds.height * 0.4)
+                $0.minimumLineSpacing = CGFloat(Constant.offsetFromHorizon)
+                $0.minimumInteritemSpacing = 0
+                $0.scrollDirection = .horizontal
+            }
+            
+            $0.delegate = self
+            $0.dataSource = self
+            $0.register(TodayMovieContentCell.self)
+            $0.collectionViewLayout = layout
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(TodayMovieContentCell.self, for: indexPath)
+        
+        return cell
+    }
+}
+// MARK: -
+
+// MARK: -ContentCell-
+final class TodayMovieContentCell: UICollectionViewCell, IsIdentifiable {
+    let posterImageView = UIImageView()
+    let titleLabel = UILabel()
+    let likeButton = UIButton()
+    let plotLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureSubview()
+        configureLayout()
+        configureView()
+    }
+    
+    private func configureSubview() {
+        contentView.addSubviews(posterImageView, titleLabel, likeButton, plotLabel)
+    }
+    
+    private func configureLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.6)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(posterImageView.snp.bottom)
+            $0.leading.equalToSuperview()
+        }
+        
+        likeButton.snp.makeConstraints {
+            $0.top.equalTo(posterImageView.snp.bottom)
+            $0.trailing.equalToSuperview()
+        }
+        
+        plotLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.lessThanOrEqualToSuperview()
+        }
+    }
+    
+    private func configureView() {
+        posterImageView.backgroundColor = .red
+        
+        titleLabel.do {
+            $0.text = "기생충"
+        }
+        
+        likeButton.do {
+            $0.setImage(UIImage(systemName: "heart"), for: .normal)
+            $0.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+        }
+        
+        plotLabel.do {
+            if Bool.random() {
+                $0.text = "이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵이러쿵저러쿵요러쿵"
+            }
+            else {
+                $0.text = "이러쿵"
+            }
+            $0.numberOfLines = 3
+        }
+    }
+}
+// MARK: -
