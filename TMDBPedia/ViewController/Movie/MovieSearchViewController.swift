196676516cb5282ab85af96e0a9d2ba2c74ba82c
songmoro <wlqgkr@naver.com>
2025-08-02 16:27:56 +0900

    셀 기본 디자인

--- TMDBPedia/ViewController/Movie/MovieSearchViewController.swift
+++ TMDBPedia/ViewController/Movie/MovieSearchViewController.swift
@@ -10,6 +10,76 @@ import Alamofire
 import SnapKit
 import Then
 
+final class SearchMovieCell: UITableViewCell, IsIdentifiable {
+    let posterImageView = UIImageView()
+    let titleLabel = UILabel()
+    let dateLabel = UILabel()
+//    let genreLables = [UILabel]()
+    let likeButton = UIButton()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        configureSubview()
+        configureLayout()
+        configureView()
+    }
+    
+    private func configureSubview() {
+        contentView.addSubviews(posterImageView, titleLabel, dateLabel, likeButton)
+    }
+    
+    private func configureLayout() {
+        posterImageView.snp.makeConstraints {
+            $0.leading.verticalEdges.equalToSuperview().inset(Constant.offsetFromHorizon).priority(1000)
+            $0.width.equalToSuperview().multipliedBy(0.25).priority(999)
+            $0.height.equalTo(posterImageView.snp.width).multipliedBy(1.25)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(Constant.offsetFromHorizon)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(Constant.offsetFromHorizon)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(Constant.offsetFromTop)
+            $0.leading.equalTo(posterImageView.snp.trailing).offset(Constant.offsetFromHorizon)
+        }
+        
+        likeButton.snp.makeConstraints {
+            $0.trailing.bottom.equalToSuperview().inset(Constant.offsetFromHorizon)
+        }
+    }
+    
+    private func configureView() {
+        posterImageView.do {
+            $0.backgroundColor = .Fill
+            $0.layer.cornerRadius = Constant.defaultRadius
+        }
+        
+        titleLabel.do {
+            $0.text = Bool.random() ? "세상에서 가장 긴 영화 제목이 만약에 존재한다면 두 줄로 보일 수 있겠지" : "영화 제목"
+            $0.numberOfLines = 2
+        }
+        
+        dateLabel.do {
+            $0.text = "2024. 04. 25"
+        }
+        
+        likeButton.do {
+            $0.setImage(UIImage(systemName: "heart"), for: .normal)
+            $0.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+        }
+    }
+}
+
 // MARK: -MovieSearchViewController-
 final class MovieSearchViewController: UIViewController {
     private let searchBar = UISearchBar()
@@ -20,6 +90,8 @@ final class MovieSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        
+        callSearchMovieAPI("어벤")
     }
 }
 // MARK: -Configure-
@@ -96,7 +168,8 @@ private extension MovieSearchViewController {
     }
     
     private func handleSuccess(_ response: SearchMovieResponse) {
-        print(response)
+        movieInfo = response
+        tableView.reloadData()
     }
 }
 // MARK: -TableView-
@@ -104,14 +177,15 @@ extension MovieSearchViewController: UITableViewDelegate, UITableViewDataSource 
     private func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(SearchMovieCell.self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        movieInfo.results.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(SearchMovieCell.self, for: indexPath)
         
         return cell
     }
