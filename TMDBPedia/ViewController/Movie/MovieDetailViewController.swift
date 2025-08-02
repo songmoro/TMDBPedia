f83ebca7bc1698928aaf5e8b72697f76cd164da1
songmoro <wlqgkr@naver.com>
2025-08-03 00:10:07 +0900

    무비 상세 정보 뷰컨트롤러 생성

---
+++ TMDBPedia/ViewController/Movie/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  TMDBPedia
+//
+//  Created by 송재훈 on 8/3/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+// MARK: -MovieDetailViewController-
+final class MovieDetailViewController: UIViewController {
+    
+}
+// MARK: -
