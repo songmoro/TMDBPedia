b064b440c090fcb547f1294bb20ae9e02bb05590
songmoro <wlqgkr@naver.com>
2025-08-03 22:01:04 +0900

    탈퇴 기능 구현

--- TMDBPedia/ViewController/Settings/SettingsViewController.swift
+++ TMDBPedia/ViewController/Settings/SettingsViewController.swift
@@ -11,34 +11,148 @@ import Then
 
 // MARK: -SettingsViewController-
 final class SettingsViewController: UIViewController {
+    private let headerView = UIView()
+    private let nicknameLabel = UILabel()
+    private let registerDateLabel = UILabel()
+    private let chevronImageView = UIImageView()
+    private let storageButton = UIButton()
+    
+    private let tableView = UITableView()
+    private let list = [
+        "자주 묻는 질문",
+        "1:1 문의",
+        "알림 설정",
+        "탈퇴하기"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
 }
 // MARK: -Configure-
-extension SettingsViewController {
+private extension SettingsViewController {
     private func configure() {
         configureSubview()
         configureLayout()
         configureView()
         configureNavigation()
+        configureTableView()
     }
     
     private func configureSubview() {
-        
+        headerView.addSubviews(nicknameLabel, registerDateLabel, chevronImageView, storageButton)
+        view.addSubviews(headerView, tableView)
     }
     
     private func configureLayout() {
+        headerView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview(\.safeAreaLayoutGuide).inset(Constant.offsetFromHorizon)
+            $0.height.equalTo(100)
+        }
         
+        nicknameLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(Constant.offsetFromHorizon)
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.verticalEdges.equalTo(nicknameLabel)
+            $0.trailing.equalToSuperview().inset(Constant.offsetFromHorizon)
+        }
+        
+        registerDateLabel.snp.makeConstraints {
+            $0.verticalEdges.equalTo(nicknameLabel)
+            $0.trailing.equalTo(chevronImageView.snp.leading).inset(-Constant.offsetFromHorizon)
+        }
+        
+        storageButton.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(Constant.offsetFromHorizon)
+            $0.horizontalEdges.bottom.equalToSuperview().inset(Constant.semiOffsetFromHorizon)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(Constant.offsetFromRelated)
+            $0.horizontalEdges.bottom.equalToSuperview(\.safeAreaLayoutGuide)
+        }
     }
     
     private func configureView() {
         view.backgroundColor = .Background
+        
+        headerView.do {
+            $0.backgroundColor = .systemGray5
+            $0.layer.cornerRadius = Constant.defaultRadius
+        }
+        
+        nicknameLabel.do {
+            $0.text = UserDefaults.standard.string(forKey: "nickname") ?? "닉네임 로딩 실패"
+            $0.textColor = .Label
+            $0.font = .systemFont(ofSize: Constant.headerSize, weight: .semibold)
+        }
+        
+        chevronImageView.do {
+            $0.image = UIImage(systemName: "chevron.right")
+            $0.tintColor = .Fill
+        }
+        
+        registerDateLabel.do {
+            $0.text = "25.06.24 가입"
+            $0.textColor = .Fill
+            $0.font = .systemFont(ofSize: Constant.bodySize, weight: .light)
+        }
+        
+        storageButton.do {
+            var configuration = UIButton.Configuration.bordered()
+            configuration.baseBackgroundColor = .Tint.withAlphaComponent(0.3)
+            configuration.baseForegroundColor = .Label
+            configuration.title = "0개의 무비박스 보관중"
+            
+            $0.configuration = configuration
+        }
     }
     
     private func configureNavigation() {
         navigationItem.title = "설정"
+    }
+}
+// MARK: -TableView-
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    private func configureTableView() {
+        tableView.do {
+            $0.delegate = self
+            $0.dataSource = self
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 3 {
+            handleWithdrawFromAccount()
+        }
+    }
+    
+    private func handleWithdrawFromAccount() {
+        UIAlertController(title: "탈퇴하기", message: "탈퇴를 하면 데이터가 모두 초기화됩니다. 탈퇴 하시겠습니까?", preferredStyle: .alert).then {
+            $0.addAction(UIAlertAction(title: "취소", style: .cancel))
+            $0.addAction(UIAlertAction(title: "확인", style: .destructive, handler: withdraw))
+        }
+        .do {
+            present($0, animated: true)
+        }
+    }
+    
+    private func withdraw(_: UIAlertAction) {
+        UserDefaults.standard.removeObject(forKey: "nickname")
     }
 }
 // MARK: -
