185651f4a247e5738355a58c648889e02d6d2337
songmoro <wlqgkr@naver.com>
2025-08-05 01:09:23 +0900

    페이지 컨트롤 추가

--- TMDBPedia/View/Cell/MovieDetail/BackdropsCell.swift
+++ TMDBPedia/View/Cell/MovieDetail/BackdropsCell.swift
@@ -13,6 +13,7 @@ import Then
 final class BackdropsCell: BaseTableViewCell {
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
     private var backdrops = [BackdropsItem]()
+    private let pageControl = UIPageControl()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -23,6 +24,7 @@ final class BackdropsCell: BaseTableViewCell {
 extension BackdropsCell {
     public func input(_ items: [BackdropsItem]) {
         self.backdrops = items
+        pageControl.numberOfPages = min(backdrops.count, 5)
         collectionView.reloadData()
     }
 }
@@ -31,11 +33,12 @@ private extension BackdropsCell {
     private func configure() {
         configureSubview()
         configureLayout()
+        configureView()
         configureCollectionView()
     }
     
     private func configureSubview() {
-        contentView.addSubview(collectionView)
+        contentView.addSubviews(collectionView, pageControl)
     }
     
     private func configureLayout() {
@@ -44,19 +47,28 @@ private extension BackdropsCell {
             $0.height.equalTo(collectionView.snp.width).multipliedBy(0.8).priority(999)
             $0.edges.equalToSuperview().priority(998)
         }
+        
+        pageControl.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.horizontalEdges.bottom.equalToSuperview(\.safeAreaLayoutGuide)
+        }
     }
     
     private func configureView() {
-        
+        pageControl.do {
+            $0.currentPage = 0
+            $0.numberOfPages = min(backdrops.count, 5)
+        }
     }
 }
 // MARK: -CollectionView-
-extension BackdropsCell: UICollectionViewDelegate, UICollectionViewDataSource {
+extension BackdropsCell: UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
     private func configureCollectionView() {
         collectionView.do {
             $0.isPagingEnabled = true
             $0.delegate = self
             $0.dataSource = self
+            $0.showsHorizontalScrollIndicator = false
             $0.register(BackdropCell.self)
         }
     }
@@ -69,7 +81,6 @@ extension BackdropsCell: UICollectionViewDelegate, UICollectionViewDataSource {
     private func updateCollectionViewLayout() {
         let layout = UICollectionViewFlowLayout().then {
             let cellBounds = self.bounds
-            
             $0.itemSize = cellBounds.size
             $0.minimumInteritemSpacing = 0
             $0.minimumLineSpacing = 0
@@ -85,12 +96,18 @@ extension BackdropsCell: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell
+        
         cell = collectionView.dequeueReusableCell(BackdropCell.self, for: indexPath).then {
             let item = backdrops[indexPath.item]
             $0.input(item.file_path)
         }
         
         return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let index = Int(round(scrollView.contentOffset.x / scrollView.frame.size.width))
+        pageControl.currentPage = index
     }
 }
 // MARK: -
