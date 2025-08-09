d6d45c4d5283f685729693b36fbfa914c6cb09a9
songmoro <wlqgkr@naver.com>
2025-08-09 12:49:31 +0900

    settable mutating self 추가

--- TMDBPedia/Protocol/Settable.swift
+++ TMDBPedia/Protocol/Settable.swift
@@ -9,9 +9,15 @@
 public protocol Settable { }
 
 extension Array: Settable {
-    func set<V>(_ to: ReferenceWritableKeyPath<Self.Element, V>, _ be: V) {
-        for i in self.indices {
+    func set<V>(_ to: ReferenceWritableKeyPath<Element, V>, _ be: V) {
+        for i in indices {
             self[i][keyPath: to] = be
+        }
+    }
+    
+    mutating func set(_ be: Element) {
+        for i in indices {
+            self[i] = be
         }
     }
 }
